2bf3b75d6fd4c2b8007d6dc0a03e4f23fead2c6a
98spiritdivine <60680118+98spiritdivine@users.noreply.github.com>
2024-07-29 11:46:15 -0400

    Update home.ejs

--- routes/home.js
+++ routes/home.js
@@ -1,11 +1,326 @@
-const express = require("express");
-const router = express.Router();
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 
-router.get("/", async (req, res, next) => {
-  return res.status(200).json({
-    title: "Express Testing",
-    message: "The app is working properly!",
-  });
-});
 
-module.exports = router;
+
+
+    <title>Bootstrap Example</title>
+    <link rel="stylesheet" href="/css/Homepage.css">
+
+</head>
+<body>
+    
+                            <%- include('./partials/Header.ejs') %>
+
+    <section id="title" class="gradient-background">
+
+        <div class="container col-xxl-8 px-4 pt-5"  >
+            <div class="row flex-lg-row-reverse align-items-center g-5 pt-5"  >
+                <div class="col-10 col-sm-8 col-lg-6"   >
+                    <div class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" height="200" loading="lazy"  >
+<!-- <div id="carouselExampleAutoplaying"  data-bs-target="#carouselExampleAutoplaying" class="carousel slide"   data-intrveal="100"> -->
+    <div id="carouselExampleAutoplaying" class="carousel slide" data-ride="carousel" data-interval="10000">
+
+    <div class="carousel-inner">
+        <section class="d-block" data-bs-target="#carouselExampleAutoplaying"  >
+<div id="carouselExampleAutoplaying" class="carousel slide pointer-event" data-bs-ride="carousel"  >
+    <div class="carousel-inner">
+        <div class="carousel-item">
+            <img class="d-block"
+                src="/css/imgs/B3.jpg">              
+            </img>
+        </div>
+<!-- ============== -->
+<!-- <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" data-interval="100"> -->
+
+
+        <div class="carousel-item active">
+            <img class="d-block" src="/css/imgs/B1.jpg" >            
+            </img>
+        </div>
+        <div class="carousel-item"   >
+            <img class="d-block" src="/css/imgs/B2.jpg">
+            </img>
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
+        data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" color="dark"   data-bs-target="#carouselExampleAutoplaying"
+        data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
+</div>
+</div>
+
+</div>
+</div>
+
+
+<!-- =================================== -->
+
+
+      
+        
+                <div class="col-lg-6">
+                    <h1 class="display-5 fw-bold lh-1  mt-20 ">You are Welcome to Donadee Internaltion School </h1>
+                    <p class="lead">Where determination leads to Success....... the world’s .most
+                        popular
+                        front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive
+                        prebuilt
+                        components, and powerful JavaScript plugins.</p>
+        
+        
+                    <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                        <button type="button" class="btn btn-light btn-lg px-4 me-md-2"><svg xmlns="http://www.w3.org/2000/svg"
+                                width="16" height="16" fill="currentColor" class="bi bi-apple mb-1" viewBox="0 0 16 16">
+                                <path
+                                    d="M11.182.008C11.148-.03 9.923.023 8.857 1.18c-1.066 1.156-.902 2.482-.878 2.516.024.034 1.52.087 2.475-1.258.955-1.345.762-2.391.728-2.43Zm3.314 11.733c-.048-.096-2.325-1.234-2.113-3.422.212-2.189 1.675-2.789 1.698-2.854.023-.065-.597-.79-1.254-1.157a3.692 3.692 0 0 0-1.563-.434c-.108-.003-.483-.095-1.254.116-.508.139-1.653.589-1.968.607-.316.018-1.256-.522-2.267-.665-.647-.125-1.333.131-1.824.328-.49.196-1.422.754-2.074 2.237-.652 1.482-.311 3.83-.067 4.56.244.729.625 1.924 1.273 2.796.576.984 1.34 1.667 1.659 1.899.319.232 1.219.386 1.843.067.502-.308 1.408-.485 1.766-.472.357.013 1.061.154 1.782.539.571.197 1.111.115 1.652-.105.541-.221 1.324-1.059 2.238-2.758.347-.79.505-1.217.473-1.282Z" />
+                                <path
+                                    d="M11.182.008C11.148-.03 9.923.023 8.857 1.18c-1.066 1.156-.902 2.482-.878 2.516.024.034 1.52.087 2.475-1.258.955-1.345.762-2.391.728-2.43Zm3.314 11.733c-.048-.096-2.325-1.234-2.113-3.422.212-2.189 1.675-2.789 1.698-2.854.023-.065-.597-.79-1.254-1.157a3.692 3.692 0 0 0-1.563-.434c-.108-.003-.483-.095-1.254.116-.508.139-1.653.589-1.968.607-.316.018-1.256-.522-2.267-.665-.647-.125-1.333.131-1.824.328-.49.196-1.422.754-2.074 2.237-.652 1.482-.311 3.83-.067 4.56.244.729.625 1.924 1.273 2.796.576.984 1.34 1.667 1.659 1.899.319.232 1.219.386 1.843.067.502-.308 1.408-.485 1.766-.472.357.013 1.061.154 1.782.539.571.197 1.111.115 1.652-.105.541-.221 1.324-1.059 2.238-2.758.347-.79.505-1.217.473-1.282Z" />
+                            </svg> Download</button>
+                        <button type="button" class="btn btn-outline-light btn-lg px-4"><svg xmlns="http://www.w3.org/2000/svg"
+                                width="16" height="16" fill="currentColor" class="bi bi-google-play mb-1" viewBox="0 0 16 16">
+                                <path
+                                    d="M14.222 9.374c1.037-.61 1.037-2.137 0-2.748L11.528 5.04 8.32 8l3.207 2.96 2.694-1.586Zm-3.595 2.116L7.583 8.68 1.03 14.73c.201 1.029 1.36 1.61 2.303 1.055l7.294-4.295ZM1 13.396V2.603L6.846 8 1 13.396ZM1.03 1.27l6.553 6.05 3.044-2.81L3.333.215C2.39-.341 1.231.24 1.03 1.27Z" />
+                            </svg> Download</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <section id="title">
+        <div class="p-5 text-center  rounded-3">
+            <svg class="bi mt-4 mb-3"
+                style="color: var(--bs-indigo); --darkreader-inline-color: var(--darkreader-text--bs-indigo, #dcdbd8);"
+                width="100" height="100" data-darkreader-inline-color="">
+                <use xlink:href="#bootstrap"></use>
+            </svg>
+            <h1 class="text-body-emphasis ">Jumbotron with icon</h1>
+            <p class="col-lg-8 mx-auto fs-5 text-muted">
+                This is a custom jumbotron featuring an SVG image at the top, some longer text that wraps early thanks
+                to a
+                responsive <code>.col-*</code> class, and a customized call to action.
+            </p>
+            <div class="d-inline-flex gap-2 mb-5">
+                <button class="d-inline-flex align-items-center btn btn-primary btn-lg px-4 rounded-pill" type="button">
+                    Call to action
+                    <svg class="bi ms-2" width="24" height="24">
+                        <use xlink:href="#arrow-right-short"></use>
+                    </svg>
+                </button>
+                <button class="btn btn-outline-secondary btn-lg px-4 rounded-pill" type="button">
+                    Secondary link
+                </button>
+            </div>
+        </div>
+
+        <div class="container">
+            <p><a href="https://elvisagyemang.org/" target="_blank" rel="noopener">Pastor Agyemang Elvis</a> is the
+                Convener
+                of
+                the prayer revival movement, Alpha Hour.&nbsp;&nbsp;Alpha Hour broadcasts live every weekday on <a
+                    href="https://web.facebook.com/PastorAgyemangElvis" target="_blank" rel="noopener">Facebook</a>, <a
+                    href="https://www.youtube.com/channel/UCtSyweTUP8dhTWfsybLFdRw" target="_blank"
+                    rel="noopener">YouTube</a>, <a href="https://www.instagram.com/rev_elvis_agyemang/" target="_blank"
+                    rel="noopener">Instagram</a>, Zoom &amp; <a href="https://www.tiktok.com/@psagyemangelvisofficial"
+                    target="_blank" rel="noopener">TikTok</a>. And also
+                broadcast television and radio on; Alpha TV Genesis TV, Onua TV, And all our 60 affiliate radio stations
+                nationwide.
+                His ministry is marked with signs, wonders and testimonies of God’s goodness.&nbsp;&nbsp;He is also
+                known
+                for his
+                teachings on marriage and relationships under the Love Clinic ministry. </p>
+        </div>
+
+    </section>
+
+    <!-- Features -->
+    <section id="features">
+
+        <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
+            <div class="col d-flex align-items-start">
+                <div class="icon-square bg-light text-dark flex-shrink-0 me-3">
+                    <img src="./images/instaSvg.svg" height="65" width="65" fill="currentColor"
+                        class="bi bi-check2-circle">
+                    </img>
+                </div>
+                <div>
+                    <h2>Featured title</h2>
+                    <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another
+                        sentence
+                        and
+                        probably just keep going until we run out of words.</p>
+                    <a href="#" class="btn btn-primary">
+                        Primary button
+                    </a>
+                </div>
+            </div>
+            <div class="col d-flex align-items-start">
+                <div class="icon-square bg-light text-dark flex-shrink-0 me-3">
+                    <svg class="bi" width="1em" height="1em">
+                        <use xlink:href="#cpu-fill"></use>
+                    </svg>
+                </div>
+                <div>
+                    <h2>Featured title</h2>
+                    <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another
+                        sentence
+                        and
+                        probably just keep going until we run out of words.</p>
+                    <a href="#" class="btn btn-primary">
+                        Primary button
+                    </a>
+                </div>
+            </div>
+            <div class="col d-flex align-items-start">
+                <div class="icon-square bg-light text-dark flex-shrink-0 me-3">
+                    <svg class="bi" width="1em" height="1em">
+                        <use xlink:href="#tools"></use>
+                    </svg>
+                </div>
+                <div>
+                    <h2>Featured title</h2>
+                    <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another
+                        sentence
+                        and
+                        probably just keep going until we run out of words.</p>
+                    <a href="#" class="btn btn-primary">
+                        Primary button
+                    </a>
+                </div>
+            </div>
+        </div>
+        </div>
+    </section>
+
+    <!-- ==========Spotify link============= -->
+    <div class="position-relative p-5 text-center text-muted bg-body border border-dashed rounded-5">
+        <button type="button"
+            class="position-absolute top-0 end-0 p-3 m-3 btn-close bg-secondary bg-opacity-10 rounded-pill"
+            aria-label="Close"></button>
+        <svg class="bi mt-5 mb-3" width="48" height="48">
+            <use xlink:href="#check2-circle"></use>
+        </svg>
+        <h1 class="text-body-emphasis">Placeholder jumbotron</h1>
+        <p class="col-lg-6 mx-auto mb-4">
+            This faded back jumbotron is useful for placeholder content. It's also a great way to add a bit of context
+            to a
+            page or section when no content is available and to encourage visitors to take a specific action.
+        </p>
+        <button class="btn btn-primary px-5 mb-5" type="button">
+            Call to action
+        </button>
+    </div>
+
+    <!-- Features -->
+    <section id="features">
+
+
+    </section>
+    <!-- Testimonial -->
+    <section id="testimonial">
+
+    </section>
+    <!-- Pricing -->
+    <section id="pricing">
+
+    </section>
+    <!-- Footer -->
+    <section id="footer">
+        <div class="container">
+            <footer class="py-5">
+                <div class="row">
+                    <div class="col-6 col-md-2 mb-3">
+                        <h5>Section</h5>
+                        <ul class="nav flex-column">
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-6 col-md-2 mb-3">
+                        <h5>Section</h5>
+                        <ul class="nav flex-column">
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-6 col-md-2 mb-3">
+                        <h5>Section</h5>
+                        <ul class="nav flex-column">
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-md-5 offset-md-1 mb-3">
+                        <form>
+                            <h5>Subscribe to our newsletter</h5>
+                            <p>Monthly digest of what's new and exciting from us.</p>
+                            <div class="d-flex flex-column flex-sm-row w-100 gap-2">
+                                <label for="newsletter1" class="visually-hidden">Email address</label>
+                                <input id="newsletter1" type="text" class="form-control" placeholder="Email address">
+                                <button class="btn btn-primary" type="button">Subscribe</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+                <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+                    <p>© 2022 Company, Inc. All rights reserved.</p>
+                    <ul class="list-unstyled d-flex">
+                        <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24">
+                                    <use xlink:href="#twitter"></use>
+                                </svg></a></li>
+                        <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24">
+                                    <use xlink:href="#instagram"></use>
+                                </svg></a></li>
+                        <li class="ms-3"><a class="link-dark" href="#">
+                                <p> Phone Us</p>
+                                <svg aria-hidden="true" focusable="false" class="icon"
+                                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="1em"
+                                    height="1em"><!-- Font Awesome Free 5.15.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) -->
+                                    <path
+                                        d="M493.4 24.6l-104-24c-11.3-2.6-22.9 3.3-27.5 13.9l-48 112c-4.2 9.8-1.4 21.3 6.9 28l60.6 49.6c-36 76.7-98.9 140.5-177.2 177.2l-49.6-60.6c-6.8-8.3-18.2-11.1-28-6.9l-112 48C3.9 366.5-2 378.1.6 389.4l24 104C27.1 504.2 36.7 512 48 512c256.1 0 464-207.5 464-464 0-11.2-7.7-20.9-18.6-23.4z" />
+                                </svg>
+                            </a></li>
+
+
+                    </ul>
+                </div>
+            </footer>
+        </div>
+
+    </section>
+
+
+</body>
+</html>
+
+
+    
+
+
+
+
